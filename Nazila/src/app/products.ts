326b2b410693872faaab70d53616306125e8c327
Nazila <azabdaftar@outlook.com>
2023-02-22 12:22:42 +0100

    fill products Array

--- Nazila/src/app/products.ts
+++ Nazila/src/app/products.ts
@@ -1,5 +1,33 @@
 export const products = [
-    { name:"", category:"", pic:"", dec:"", Price:10},
-   
-
+    {
+        name: "Shelduck, common",
+        category: "Tadorna tadorna",
+        pic: ".png",
+        description: "Toxic effect of soaps, assault",
+        price: 51
+      }, {
+        name: "Eleven-banded armadillo (unidentified)",
+        category: "Cabassous sp.",
+        pic: ".png",
+        description: "Puncture wound w foreign body of eyelid and periocular area",
+        price: 37
+      }, {
+        name: "Pocket gopher (unidentified)",
+        category: "unavailable",
+        pic: ".png",
+        description: "Oth reactions and intoxications d/t drugs administered to NB",
+        price: 50
+      }, {
+        name: "Painted stork",
+        category: "Mycteria leucocephala",
+        pic: ".png", 
+        description:"Tinea pedis",
+        price: 6
+      }, {
+        name: "White-browed sparrow weaver",
+        category: "Plocepasser mahali",
+        pic: ".png",
+        description: "Driver of 3-whl mv injured in clsn w hv veh nontraf, sequela",
+        price: 45
+      }
 ];
